f68ed1c1098ca119a5194ec3de0e9965107d1087
GriefNorth <grief.north@gmail.com>
2013-03-24 16:14:18 +0400

    patch for touch driver

--- drivers/input/touchscreen/atmel_mxt165.c
+++ drivers/input/touchscreen/atmel_mxt165.c
@@ -504,9 +504,10 @@ static void atmel_mxt165_report(struct atmel_mxt165_info *ts, int i, int x, int 
             if (i == 0)
             {
 #ifdef ATMEL_MXT165_MT
-                input_report_abs(ts->touch_input, ABS_MT_TOUCH_MAJOR, 0);
-                input_report_abs(ts->touch_input, ABS_MT_POSITION_X, x);
-                input_report_abs(ts->touch_input, ABS_MT_POSITION_Y, y);
+            input_report_abs(ts->touch_input, ABS_X, x);
+            input_report_abs(ts->touch_input, ABS_Y, y);
+            input_report_abs(ts->touch_input, ABS_PRESSURE, pressure);
+            input_report_key(ts->touch_input, BTN_TOUCH, 0);
 #else
                 input_report_abs(ts->touch_input, ABS_X, x);
                 input_report_abs(ts->touch_input, ABS_Y, y);
